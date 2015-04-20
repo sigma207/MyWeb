8219dfd8eb7e1a6f63ee22601bd8d6740304c040
sigma207 <ktuou207@gmail.com>
2015-04-21 00:37:27 +0800

    tableFixedHeader.js first version!

--- web/js/tableFixedHeader.js
+++ web/js/tableFixedHeader.js
@@ -1,56 +1,63 @@
 /**
  * Created by user on 2015/4/20.
  */
-var tf = {
-    fixedTHead: function (headClass,scrollerClass) {
-        var tHead = $("."+headClass);
-        var scroller = $("."+scrollerClass);
+var FixedHeader = {
+    createNew: function (tableClass,scrollClass) {
+        var fh = {};
+        fh.table = $("."+tableClass);
+        fh.tHead = fh.table.find("thead");
+        fh.scroll = $("."+scrollClass);
+        fh.tHeadHolder;
+        fh.init = function(){
+            fh.scroll.on("scroll", function (e) {
+                fh.updatePosition();
+            });
 
-        fixHeader();
+            $( window ).resize(function() {
+                fh.fixHeader();
+            });
 
-        scroller.on("scroll", function (e) {
-            updatePosition();
-        });
+        };
 
-        $( window ).resize(function() {
-            fixHeader();
-        });
-
-        function fixHeader(){
-            //要修正resize bug..
-            tHead.css("position","static");
-            var headTr = tHead.find("tr").eq(0);
-            var thList = headTr.children();
-            var tHeadHolder = $(".tHeadHolder");
+        fh.fixHeader = function(){
+            var thList = fh.tHead.find("tr").eq(0).children();
+            var thHolderList = fh.tHeadHolder.find("tr").eq(0).children();
             /**
-             * 如果table 的datasource改變 可能會讓欄位增加或減少
-             * 此時要把tHeadHolder remover後再重建
+             * very important, make sure the two tHead have same width.
+             * or tHead won't have enough width to put width of all tds.
              */
-            tHeadHolder.remove();
-
-            tHead.after("<thead class='tHeadHolder'>"+tHead.html()+"</thead>");
-
-
+            fh.tHead.css("width",fh.tHeadHolder.css("width"));//very important, make sure the two tHead have same width.
             thList.each(function(index){
-                $(this).attr("originalWidth",$(this).css("width"));
+                $(this).css("width",thHolderList.eq(index).css("width"));
             });
+            fh.updatePosition();
+        };
 
-            tHead.css("position","absolute");
-
-            thList.each(function(index){
-                $(this).css("width",$(this).attr("originalWidth"));
-            });
-            updatePosition();
-        }
-
-        function updatePosition(){
-            var tHeadHeight = parseInt(tHead.css("height"));
-            var scrollTop = scroller.scrollTop();
-            if(scrollTop>0){
-                tHead.css("transform","translate(0px,"+(parseInt(scroller.scrollTop())-tHeadHeight-1)+"px)");//firefox chrome
-            }else{
-                tHead.css("transform","translate(0px,"+(-tHeadHeight)+"px)");//firefox chrome
+        /**
+         * call this function when init and table set dataSource(column might change)
+         */
+        fh.createHolder = function(){
+            fh.tHead.css("position","static");
+            if (typeof fh.tHeadHolder !== typeof undefined && fh.tHeadHolder !== false) {
+                fh.tHeadHolder.remove();
             }
-        }
+
+            fh.tHead.after("<thead class='tHeadHolder'>"+fh.tHead.html()+"</thead>");
+            fh.tHeadHolder = $(".tHeadHolder");
+            fh.tHead.css("position","absolute");
+        };
+
+        fh.updatePosition = function(){
+            var tHeadHeight = parseInt(fh.tHead.css("height"));
+            var scrollTop = parseInt(fh.scroll.scrollTop());
+            if(scrollTop>0){
+                fh.tHead.css("transform","translate(0px,"+(scrollTop-tHeadHeight-1)+"px)");
+            }else{
+                fh.tHead.css("transform","translate(0px,"+(-tHeadHeight)+"px)");
+            }
+        };
+
+        fh.init();
+        return fh;
     }
 };
