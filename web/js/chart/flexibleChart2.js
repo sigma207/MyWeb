cde15ec382220b52296418da1699356dc125bff4
sigma207 <ktuou207@gmail.com>
2015-05-27 17:08:40 +0800

    flexibleChart2.js

---
+++ web/js/chart/flexibleChart2.js
@@ -0,0 +1,466 @@
+/**
+ * Created by user on 2015/4/25.
+ */
+var Chart = {
+    createNew: function (canvasDom) {
+        var chart = {};
+        chart.canvas = canvasDom;
+        //chart.ctx = chart.canvas.getContext("2d");//不需要知道ctx的存在
+        chart.PADDING_BOTTOM = 0;
+        chart.PADDING_TOP = 0;
+        chart.PADDING_LEFT = 0;
+        chart.PADDING_RIGHT = 0;
+        chart.layerManager = LayerManager.createNew();
+        chart.padding = function () {
+            if (arguments.length == 1) {
+                var padding = arguments[0];
+                chart.PADDING = padding;
+                chart.PADDING_LEFT = padding;
+                chart.PADDING_BOTTOM = padding;
+                chart.PADDING_RIGHT = padding;
+                chart.PADDING_TOP = padding;
+            } else if (arguments.length == 4) {
+                chart.PADDING_LEFT = arguments[0];
+                chart.PADDING_BOTTOM = arguments[1];
+                chart.PADDING_RIGHT = arguments[2];
+                chart.PADDING_TOP = arguments[3];
+            }
+        };
+
+        chart.setDataSource = function (source, listName) {
+            chart.dataDriven = DataDriven.createNew(chart, source, listName);
+            return chart.dataDriven;
+        };
+
+        chart.windowToCanvas = function (x, y) {
+            var bbox = canvas.getBoundingClientRect();
+            return {
+                x: x - bbox.left,
+                y: y - bbox.top
+            };
+        };
+
+        chart.layerManager.addBaseLayer(canvasDom);
+        return chart;
+    }
+};
+
+var DataDriven = {
+    createNew: function (chart, source, listName) {
+        var dataDriven = {};
+        dataDriven.chart = chart;
+        dataDriven.listName = listName;
+        dataDriven.reset = function (newData) {
+            dataDriven.source = newData;
+            dataDriven.list = source[listName];
+            dataDriven.count = dataDriven.list.length;
+        };
+        dataDriven.reset(source);
+        return dataDriven;
+    }
+};
+
+var CanvasLayer = {
+    createNew: function (id, canvas) {
+        var layer = {};
+        layer.id = id;
+        layer.canvas = canvas;
+        layer.ctx = canvas.getContext("2d");
+        layer.clear = function () {
+            layer.ctx.clearRect(0, 0, layer.canvas.width, layer.canvas.height);
+        };
+        return layer;
+    }
+};
+
+var LayerManager = {
+    createNew: function () {
+        var cm = {};
+        cm.layerList = [];
+        cm.baseLayer = undefined;
+        cm.addBaseLayer = function (canvas) {
+            cm.baseLayer = CanvasLayer.createNew("baseLayer", canvas);
+            $(cm.baseLayer.canvas).css("position", "absolute");
+            cm.layerList.push(cm.baseLayer);
+        };
+        cm.addLayer = function (id) {
+            var canvas = document.createElement('canvas');
+            var layer = CanvasLayer.createNew(id, canvas);
+            var layerCanvas = $(layer.canvas);
+            var baseLayer = $(cm.baseLayer.canvas);
+            baseLayer.parent().append(layerCanvas);
+            layerCanvas.attr("id", id);
+            layerCanvas.attr("width", baseLayer.attr("width"));
+            layerCanvas.attr("height", baseLayer.attr("height"));
+            layerCanvas.css("position", "absolute");
+            layerCanvas.css("opacity", "1");
+
+            cm.layerList.push(layer);
+            return layer;
+        };
+        cm.getLayer = function (index) {
+            return cm.layerList[index];
+        };
+        cm.getLayerById = function (id) {
+            for (var i = 0; i < cm.layerList.length; i++) {
+                if (cm.layerList[i].id == id) {
+                    return cm.layerList[i];
+                }
+            }
+        };
+        cm.clearLayer = function (index) {
+            cm.getLayer(index).clear();
+        };
+
+        return cm;
+    }
+};
+
+var CanvasComponent = {
+    createNew: function () {
+        var cc = {};
+        cc.drawContextList = [];
+        cc.addLayerDrawFunction = function (index, drawFunction) {
+            if (typeof cc.drawContextList[index] === typeof undefined) {
+                cc.drawContextList[index] = [];
+            }
+            cc.drawContextList[index].push(drawFunction);
+        };
+        cc.drawChartLayer = function (chart, index) {
+            var context = chart.layerManager.getLayer(index).ctx;
+            var drawFunctionList = cc.drawContextList[index];
+            for (var i = 0; i < drawFunctionList.length; i++) {
+                drawFunctionList[i].call(cc, context);
+            }
+        };
+        return cc;
+    }
+};
+
+var ChartMouse = {
+    createNew: function (chart, mouseLayer) {
+        var mouse = CanvasComponent.createNew();
+        mouse.layer = mouseLayer;
+        mouse.x = undefined;
+        mouse.y = undefined;
+        mouse.lastX = undefined;
+        mouse.lastY = undefined;
+        mouse.dragging = false;
+        mouse.onMoveCall = undefined;
+        mouse.onDownCall = undefined;
+        mouse.onOutCall = undefined;
+        mouse.onUpCall = undefined;
+        mouse.onWheelCall = undefined;
+        mouse.registerMouseEvent = function () {
+            var canvas = $(mouse.layer.canvas);
+            canvas.mousemove(mouse.onMouseMove);
+            canvas.mousedown(mouse.onMouseDown);
+            canvas.mouseout(mouse.onMouseOut);
+            canvas.mouseup(mouse.onMouseUp);
+            canvas.mousewheel(mouse.onMouseWheel);
+        };
+
+        mouse.onMouseMove = function (e) {
+            e.preventDefault();
+            var temp = chart.windowToCanvas(e.clientX, e.clientY);
+            mouse.x = temp.x;
+            mouse.y = temp.y;
+            if (typeof mouse.onMoveCall !== typeof undefined) {
+                mouse.onMoveCall.call(mouse);
+            }
+            mouse.lastX = mouse.x;
+            mouse.lastY = mouse.y;
+        };
+
+        mouse.onMouseDown = function (e) {
+            e.preventDefault();
+            if (typeof mouse.onDownCall !== typeof undefined) {
+                mouse.onDownCall.call(mouse);
+            }
+        };
+
+        mouse.onMouseOut = function (e) {
+            if (typeof mouse.onOutCall !== typeof undefined) {
+                mouse.onOutCall.call(mouse);
+            }
+        };
+
+        mouse.onMouseUp = function (e) {
+            if (typeof mouse.onUpCall !== typeof undefined) {
+                mouse.onUpCall.call(mouse);
+            }
+        };
+
+        mouse.onMouseWheel = function (e, delta) {
+            e.preventDefault();
+            if (delta > 1) {//有可能大於1或小於-1,要強迫修正
+                delta = 1;
+            } else if (delta < -1) {
+                delta = -1;
+            }
+            if (typeof mouse.onWheelCall !== typeof undefined) {
+                mouse.onWheelCall.call(mouse, delta);
+            }
+        };
+        mouse.registerMouseEvent();
+        return mouse;
+    }
+};
+
+var Axis = {
+    createNew: function (x, y, length) {
+        var axis = CanvasComponent.createNew();
+        axis.x = x;
+        axis.y = y;
+        axis.length = length;
+        axis.column = undefined;
+        return axis;
+    }
+};
+
+var AxisX = {
+    createNew: function (x, y, width) {
+        return Axis.createNew(x, y, width);
+    }
+};
+
+//value volume
+var AxisY = {
+    createNew: function (x, y, height) {
+        var axis = Axis.createNew(x, y, height);
+        axis.height = height;
+        axis.top = axis.y - axis.height;
+        return axis;
+    }
+};
+
+var ValueAxis = {
+    createNew: function (period, column, minColumn, maxColumn, x, y, height) {
+        var axis = AxisY.createNew(x, y, height);
+        axis.period = period;
+        axis.source = axis.period.chart.dataDriven.source;
+        axis.column = column;
+        axis.minColumn = minColumn;
+        axis.maxColumn = maxColumn;
+
+        axis.valueMin = JsonTool.formatFloat(axis.source[axis.minColumn], 2);
+        axis.valueMax = JsonTool.formatFloat(axis.source[axis.maxColumn], 2);
+        axis.valueDistance = JsonTool.formatFloat(axis.valueMax - axis.valueMin, 2);
+        axis.valueScale = axis.height / axis.valueDistance;
+
+        axis.convertY = function (value) {
+            return Math.round((value - axis.valueMin) * axis.valueScale);
+        };
+
+        axis.createTicks = function (count) {
+            return AxisTicks.createNew(axis, count);
+        };
+        return axis;
+    }
+};
+
+var PeriodAxis = {
+    createNew: function (runChart, displayRange, displayRangeMin) {
+        var axis = AxisX.createNew(runChart.area.x, runChart.area.y, runChart.area.width);
+        axis.chart = runChart;
+        axis.valueAxisList = [];
+        axis.displayRange = displayRange;
+        axis.displayRangeMin = displayRangeMin;
+        axis.startIndex = runChart.dataDriven.count - axis.displayRange;
+        axis.endIndex = axis.startIndex + axis.displayRange - 1;
+        axis.scale = runChart.area.width / (axis.displayRange + 1);
+        axis.createValueAxis = function (column, minColumn, maxColumn, x, y, height) {
+            var valueAxis = ValueAxis.createNew(axis, column, minColumn, maxColumn, x, y, height);
+            axis.valueAxisList.push(valueAxis);
+            return valueAxis;
+        };
+        axis.convertX = function (index) {
+            return Math.round(axis.chart.area.x + axis.scale + (index * axis.scale));
+        };
+        axis.convertIndex = function (x) {
+            if (x < axis.chart.area.x + axis.scale) {
+                return -1;
+            } else {
+                return Math.round((x - (axis.chart.area.x + axis.scale)) / axis.scale);
+            }
+        };
+
+        /**
+         * 生成時間軸內資料的x,y
+         */
+        axis.generateDataLoc = function () {
+            var list = axis.chart.dataDriven.list;
+            var data;
+            var valueAxis;
+            var valueAxisCount = axis.valueAxisList.length;
+            for (var i = 0, timeIndex = axis.startIndex; i < axis.displayRange; i++, timeIndex++) {
+                data = list[timeIndex];
+                data.x = axis.convertX(i);
+                for (var v = 0; v < valueAxisCount; v++) {
+                    valueAxis = axis.valueAxisList[v];
+                    data[valueAxis.column + "Y"] = valueAxis.y - valueAxis.convertY(data[valueAxis.column]);
+                }
+            }
+        };
+
+        axis.changeDisplayRange = function (startIndex) {
+            axis.startIndex = startIndex;
+            axis.endIndex = axis.startIndex + axis.displayRange - 1;
+            axis.scale = axis.chart.area.width / (axis.displayRange + 1);
+        };
+
+        axis.addDisplayRange = function (addValue) {
+            var newRange = axis.displayRange + (addValue * 2);
+            var start, end;
+            if (newRange <= axis.chart.dataDriven.count) {
+                if (newRange <= axis.displayRangeMin) {
+                    newRange = axis.displayRangeMin;//最小
+                } else {
+                    //範圍內隨便你
+                }
+            } else {
+                newRange = axis.chart.dataDriven.count;//最大
+            }
+            console.log("newRange=" + newRange);
+            if (newRange != axis.displayRange) {
+                if (addValue > 0) {//addValue=1
+                    start = axis.startIndex - addValue;//30->29
+                    end = axis.endIndex + addValue;//39->40
+                } else {//addValue=-1
+                    start = axis.startIndex - addValue;//30->31
+                    end = axis.endIndex + addValue;//39->38
+                }
+            }
+
+            if (start >= 0 && end < axis.chart.dataDriven.count) {
+                axis.displayRange = newRange;
+                axis.changeDisplayRange(start);
+                return start;
+            }
+            return -1;
+        };
+        return axis;
+    }
+};
+
+var AxisTicks = {
+    createNew: function (axis, count) {
+        var axisTick = {};
+        axisTick.axis = axis;
+        axisTick.count = count;
+        axisTick.distance = axis.valueDistance / axisTick.count;
+        axisTick.tickList = [];
+        axisTick.addTick = function (value, color) {
+            var tick = {};
+            tick.value = value;
+            tick.color = color || "black";
+            axisTick.tickList.push(tick);
+        };
+        return axisTick;
+    }
+};
+
+var RunChart = {
+    createNew: function (canvasDom) {
+        var chart = Chart.createNew(canvasDom);
+        chart.area = {};
+
+        chart.layerManager.addLayer("valueLayer");
+        chart.layerManager.addLayer("mouseLayer");
+        //chart.mouseCanvas = chart.addMouseLayer("mouseLayer");
+
+        chart.init = function () {
+            chart.setArea();
+        };
+        chart.setArea = function () {
+            chart.area.x = chart.PADDING_LEFT;
+            chart.area.y = chart.canvas.height - chart.PADDING_BOTTOM;
+            chart.area.right = chart.canvas.width - chart.PADDING_RIGHT;
+            chart.area.width = chart.area.right - chart.area.x;
+            chart.area.top = chart.PADDING_TOP;
+            chart.area.height = chart.area.y - chart.area.top;
+        };
+
+        chart.createPeriodAxis = function (column, displayRange, displayRangeMin) {
+            chart.periodAxis = PeriodAxis.createNew(chart, displayRange, displayRangeMin);
+            chart.periodAxis.column = column;
+            return chart.periodAxis;
+        };
+
+        chart.draw = function () {
+            //chart.periodAxis.generateDataLoc();
+            chart.drawAxis();
+            chart.drawPeriod();
+        };
+
+        chart.drawAxis = function () {
+            chart.layerManager.clearLayer(0);
+            var periodAxis = chart.periodAxis;
+            var valueAxis;
+            for (var i = 0; i < periodAxis.valueAxisList.length; i++) {
+                valueAxis = periodAxis.valueAxisList[i];
+                valueAxis.drawChartLayer(chart, 0);
+            }
+        };
+
+        chart.drawPeriod = function () {
+            var periodAxis = chart.periodAxis;
+            periodAxis.generateDataLoc();
+            chart.layerManager.clearLayer(1);
+            periodAxis.drawChartLayer(chart, 1);
+            var valueAxis;
+            for (var i = 0; i < periodAxis.valueAxisList.length; i++) {
+                valueAxis = periodAxis.valueAxisList[i];
+                valueAxis.drawChartLayer(chart, 1);
+            }
+        };
+
+        chart.changePeriodRange = function (startIndex) {
+            chart.periodAxis.changeDisplayRange(startIndex);
+            chart.drawPeriod();
+        };
+
+        chart.createMouse = function (mouseLayer) {
+            chart.mouse = ChartMouse.createNew(chart, mouseLayer);
+            chart.mouse.onMoveCall = chart.mouseMove;
+            chart.mouse.onDownCall = chart.mouseDown;
+            chart.mouse.onOutCall = chart.mouseOut;
+            chart.mouse.onUpCall = chart.mouseUp;
+            chart.mouse.onWheelCall = chart.mouseWheel;
+            return chart.mouse;
+        };
+
+        chart.mouseMove = function () {
+            var periodAxis = chart.periodAxis;
+            chart.mouse.index = periodAxis.convertIndex(chart.mouse.x);
+            chart.mouse.inChartArea = (chart.mouse.x >= chart.area.x + periodAxis.scale && chart.mouse.x <= chart.area.right - periodAxis.scale);
+
+            chart.layerManager.clearLayer(2);
+            chart.mouse.drawChartLayer(chart, 2);
+        };
+
+        chart.mouseDown = function () {
+            if (chart.mouse.inChartArea) {
+                chart.mouse.dragging = true;
+            }
+        };
+
+        chart.mouseOut = function () {
+            chart.mouse.dragging = false;
+        };
+
+        chart.mouseUp = function () {
+            chart.mouse.dragging = false;
+        };
+
+        chart.mouseWheel = function (delta) {
+            var periodAxis = chart.periodAxis;
+            var start = periodAxis.addDisplayRange(delta);
+            if (start != -1) {
+                chart.drawPeriod();
+            }
+        };
+
+        return chart;
+    }
+};
---
+++ web/js/chart/runChartDrawStyle2.js
@@ -0,0 +1,257 @@
+/**
+ * Created by user on 2015/4/29.
+ */
+var DrawStyle = {
+    createNew: function (runChart) {
+        var ds = {};
+
+        ds.drawValueAxis = function (bgCtx) {//this = chart
+            var axis = this;
+            //console.log("drawValueAxis:%s",axis.column);
+            bgCtx.save();
+            bgCtx.drawHorizontalLine(axis.x, axis.y, runChart.area.right);
+            bgCtx.drawVerticalLine(axis.x, axis.y, axis.top);
+            bgCtx.restore();
+        };
+
+        ds.drawValueAxisTicks = function (bgCtx) {
+            var axis = this;
+            axis.ticks = ds.createValueAxisTicks(axis);
+            bgCtx.save();
+            bgCtx.textBaseline = "bottom";
+            bgCtx.textAlign = "right";
+            var valueFormat;
+            if (axis.column == "value") {
+                bgCtx.font = "10px Helvetica";
+                valueFormat = "0,0.00";
+            } else if (axis.column == "volume") {
+                bgCtx.font = "8px Helvetica";
+                valueFormat = "0,0";
+            }
+            var x = axis.x - 5;
+            var y = axis.y;
+            var tick;
+            for (var i = 0; i < axis.ticks.tickList.length; i++) {
+                tick = axis.ticks.tickList[i];
+                bgCtx.fillStyle = tick.color;
+                y = axis.y - axis.convertY(tick.value);
+                bgCtx.fillText(numeral(tick.value).format(valueFormat), x, y);
+                if (i > 0)bgCtx.drawDashLine(axis.x, y, runChart.area.right, y, 2);//在軸線上不用劃
+            }
+            bgCtx.restore();
+        };
+
+        ds.drawValueAxisData = function (ctx) {
+            var valueAxis = this;
+            console.log("drawValueAxisData:%s",valueAxis.column);
+            var periodAxis = valueAxis.period;
+            var list = runChart.dataDriven.list;
+            var data, i;
+            ctx.save();
+            if (valueAxis.column == "value") {
+                var oldValue, newValue, x, y, oldX, oldY;
+                for (i = periodAxis.startIndex; i <= periodAxis.endIndex; i++) {
+                    data = list[i];
+                    newValue = data[valueAxis.column];
+                    x = data.x;
+                    y = data[valueAxis.column + "Y"];
+                    if (i == 0) {
+                    } else {
+                        if (newValue > oldValue) {
+                            ctx.strokeStyle = "red";
+                        } else {
+                            ctx.strokeStyle = "green";
+                        }
+                        ctx.beginPath();
+                        ctx.moveTo(oldX, oldY);
+                        ctx.lineTo(x, y);
+                        ctx.stroke();
+                    }
+                    oldX = x;
+                    oldY = y;
+                    oldValue = newValue;
+                }
+            } else if (valueAxis.column == "volume") {
+                ctx.strokeStyle = "blue";
+                ctx.fillStyle = "#3E92FF";
+                var startX, endX, quarterScale = periodAxis.scale / 4;
+                ctx.beginPath();
+                for (i = periodAxis.startIndex; i <= periodAxis.endIndex; i++) {
+                    data = list[i];
+                    startX = data.x - quarterScale;
+                    endX = data.x + quarterScale;
+                    ctx.moveTo(startX, valueAxis.y);
+                    ctx.lineTo(startX, data[valueAxis.column + "Y"]);
+                    ctx.lineTo(endX, data[valueAxis.column + "Y"]);
+                    ctx.lineTo(endX, valueAxis.y);
+                }
+                ctx.stroke();
+                ctx.fill();
+            }
+            ctx.restore();
+        };
+
+        ds.drawPeriodAxisTicks = function (ctx) {
+            var axis = this;
+            ctx.save();
+            ctx.fillStyle = "black";
+            ctx.textAlign = "center";
+            var y = runChart.area.y + 10;
+            var timeIndex = 0;
+            var timeStep = 0;
+            if (axis.displayRange <= 30) {
+                timeStep = 5;//30分內 每隔5分顯示
+            } else if (axis.displayRange <= 60) {
+                timeStep = 10;//一小時內 每隔10分顯示
+            } else if (axis.displayRange <= 120) {
+                timeStep = 30;//2小時內 每隔30分顯示
+            } else {
+                timeStep = 60;//超過2小時 每隔60分顯示
+            }
+            var list = runChart.dataDriven.list;
+            for (var i = axis.startIndex; i < axis.endIndex; i += timeStep) {
+                ctx.fillText(moment(list[i][axis.column], "HHmm").format("HH:mm"), axis.convertX(timeIndex), y);
+                timeIndex += timeStep;
+            }
+            if (i != axis.endIndex - 1) {
+                //ctx.fillText(moment(drawData.valueList[info.timeEndIndex].time, "HHmm").format("HH:mm"), convertRangeX(info.displayTimeRange - 1), y);
+            }
+            ctx.restore();
+        };
+
+        ds.createValueAxisTicks = function (axis) {
+            var ticks;
+            if (axis.column == "value") {
+                ticks = axis.createTicks(6);
+                var top = axis.valueMin;
+                var i;
+                for (i = 0; i < ticks.count / 2; i++) {
+                    ticks.addTick(top, "green");
+                    top = JsonTool.formatFloat(top + ticks.distance, 2);
+                }
+                top = axis.source.close;
+                for (i = 0; i < ticks.count / 2; i++) {
+                    if (i == 0) {
+                        ticks.addTick(top, "black");
+                    } else {
+                        ticks.addTick(top, "red");
+                    }
+                    top = JsonTool.formatFloat(top + ticks.distance, 2);
+                }
+                ticks.addTick(axis.valueMax, "red");
+            } else if (axis.column == "volume") {
+                ticks = axis.createTicks(3);
+                ticks.addTick(0, "blue");
+                ticks.addTick(axis.valueMax / 2, "blue");
+                ticks.addTick(axis.valueMax, "blue");
+            }
+            return ticks;
+        };
+
+        ds.drawMouseLayerMove = function (ctx) {
+            //mouseCtx
+            var mouse = runChart.mouse;
+            var periodAxis = runChart.periodAxis;
+            if (mouse.inChartArea) {
+                if (mouse.dragging) {
+                    var newStartIndex = -1;
+                    if (mouse.x > mouse.lastX && periodAxis.endIndex < runChart.dataDriven.count - 1) {
+                        newStartIndex = periodAxis.startIndex + 1;
+                    } else if (mouse.x < mouse.lastX && periodAxis.startIndex > 0) {
+                        newStartIndex = periodAxis.startIndex - 1;
+                    }
+                    if (newStartIndex != -1) {
+                        //this.clearLayer(ctx);
+                        runChart.changePeriodRange(newStartIndex);
+                    }
+                } else {
+                    ds.mouseInfo.call(runChart,ctx);
+                }
+
+            }
+        };
+
+        ds.mouseInfo = function (ctx) {
+            var periodAxis = runChart.periodAxis;
+            var source = runChart.dataDriven.source;
+            var list = runChart.dataDriven.list;
+            var data = list[periodAxis.startIndex + runChart.mouse.index];
+            var chartArea = runChart.area;
+
+            mouseGuideWires();
+            drawValueVolumeInfo();
+
+            function mouseGuideWires() {
+                ctx.save();
+                ctx.strokeStyle = "gray";
+                ctx.drawHorizontalLine(chartArea.x, data.valueY, chartArea.right);
+                ctx.drawVerticalLine(data.x, chartArea.top, chartArea.y);
+                ctx.beginPath();
+                ctx.strokeStyle = "#FF7C00";
+                ctx.fillStyle = "white";
+                ctx.lineWidth = 2;
+                ctx.arc(data.x, data.valueY, 3, 0, Math.PI * 2, false);
+                ctx.stroke();
+                ctx.fill();
+                ctx.restore();
+            }
+
+            function drawValueVolumeInfo() {
+                ctx.save();
+
+                var variance = JsonTool.formatFloat(data.value - source.close, 2);
+                var multi = JsonTool.formatFloat(variance / source.close * 100, 2);
+                ctx.font = "12px 微軟正黑體";
+                ctx.textBaseline = "top";
+
+                var timeText = moment(data.time, "HHmm").format("HH:mm");
+                var timeMeasureText = ctx.measureText(timeText);
+
+                var valueText = data.value;
+                var valueMeasureText = ctx.measureText(valueText);
+
+                var volumeText = data.volume;
+                var volumeMeasureText = ctx.measureText(volumeText);
+
+                var varianceText = variance.toString();
+                var varianceMeasureText = ctx.measureText(varianceText);
+
+                var bgHeight = 14;
+                //時間bg
+                ctx.fillStyle = "#FF7C00";
+                var bgWidth = (timeMeasureText.width + 4);
+                ctx.fillRect(data.x - bgWidth / 2, chartArea.y, bgWidth, bgHeight);
+                //成交價bg
+                bgWidth = (valueMeasureText.width + 4);
+                ctx.fillRect(chartArea.x - bgWidth, data.valueY - bgHeight / 2, bgWidth, bgHeight);
+                //成交量bg
+                ctx.fillStyle = "blue";
+                bgWidth = (volumeMeasureText.width + 4);
+                ctx.fillRect(chartArea.x - bgWidth, data.volumeY - bgHeight / 2, bgWidth, bgHeight);
+                //漲幅bg
+                ctx.fillStyle = "#FF7C00";
+
+                var fixHeight = ($.browser.mozilla) ? 2 : -1;//2 for firefox
+                ctx.fillStyle = "white";
+                ctx.fillText(moment(data.time, "HHmm").format("HH:mm"), data.x - (timeMeasureText.width / 2), chartArea.y + fixHeight);
+                ctx.fillText(valueText, chartArea.x - (valueMeasureText.width ) - 1, data.valueY - bgHeight / 2 + fixHeight);
+                ctx.fillText(volumeText, chartArea.x - (volumeMeasureText.width ) - 1, data.volumeY - bgHeight / 2 + fixHeight);
+                if (variance > 0) {
+                    ctx.fillStyle = "red";
+                } else if (variance < 0) {
+                    ctx.fillStyle = "green";
+                } else {
+                    ctx.fillStyle = "black";
+                }
+                ctx.textAlign = "left";
+                ctx.fillText(variance, chartArea.right, data.valueY - bgHeight + fixHeight);
+                ctx.fillText(multi + "%", chartArea.right, data.valueY + fixHeight);
+
+                ctx.restore();
+
+            }
+        };
+
+        return ds;
+    }
+};
